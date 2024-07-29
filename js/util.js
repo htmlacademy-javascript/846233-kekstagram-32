92515f12e02bc7485bdc54d66d5a0375eaebb740
veronicamisko <veronicakostyunina@gmail.com>
2024-07-29 09:52:13 +0300

    Обработчики изменения фильтров

--- js/util.js
+++ js/util.js
@@ -31,10 +31,19 @@ const showAlert = (message) => {
   }, ALERT_SHOW_TIME);
 };
 
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
 
 export {
   getRandomInteger,
   checkStringLength,
   getRandomElement,
-  showAlert
+  showAlert,
+  debounce
 };
